2988e8f4a206dfcaf84ab915721bc78a153f0ad0
danielmourajc <danielmourajc@gmail.com>
2022-10-18 22:09:04 +0100

    up

--- 03 GET_NEXT_LINE/Code/get_next_line.c
+++ 03 GET_NEXT_LINE/Code/get_next_line.c
@@ -6,15 +6,74 @@
 /*   By: dmoura-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 20:13:55 by dmoura-d          #+#    #+#             */
-/*   Updated: 2022/08/02 19:36:40 by dmoura-d         ###   ########.fr       */
+/*   Updated: 2022/10/18 22:07:43 by dmoura-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*update_next_line(char *line)
+{
+	size_t	count;
+	char	*rest;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (line[count] == '\0' || line[1] == '\0')
+		return (0);
+	rest = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (*rest == '\0')
+	{
+		free(rest);
+		rest = NULL;
+	}
+	line[count + 1] = '\0';
+	return (rest);
+}
+
+static char	*get_line(int fd, char *buffer, char *rest)
+{
+	int		bytes_read;
+	char	*temp;
+
+	bytes_read = 1;
+	while (bytes_read != '\0')
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (0);
+		else if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		if (!rest)
+			rest = ft_strdup("");
+		temp = rest;
+		rest = ft_strjoin(temp, buffer);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr (buffer, '\n'))
+			break ;
+	}
+	return (rest);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	char			*line;
+	char			*buffer;
+	static char		*rest;
 
 	if	(fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (0);
+	line = get_line(fd, buffer, rest);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
 		return (NULL);
-
+	rest = update_next_line(line);
+	return (line);
 }
